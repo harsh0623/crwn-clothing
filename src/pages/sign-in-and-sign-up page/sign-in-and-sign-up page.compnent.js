3289a957336bf0417787cbbcd80973b8024a022b
harsh0623 <hv3445393@gmail.com>
2021-07-12 18:06:50 -0700

    updating sign-in component

--- src/pages/sign-in-and-sign-up page/sign-in-and-sign-up page.compnent.js
+++ src/pages/sign-in-and-sign-up page/sign-in-and-sign-up page.compnent.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 import './sign-in-and-sign-up page.styles.scss';
 
 const SigInAndSignUpPage=()=>(
  <div className='sign-in-and-sign-up page'>
     <SignIn />
+    <SignUp/>
  </div>
-)
+);
 
 export default SigInAndSignUpPage;
