974286902235525c074f4b68dc6d6b30faa3efc7
harsh0623 <hv3445393@gmail.com>
2021-07-22 17:12:16 -0700

    adding user selector and cart selector in our app

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { auth } from '../../firebase/firebase.utils';
+import { createStructuredSelector } from 'reselect';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
 
 import CartIcon from '../cart-icon/cart-icon.component';
 
@@ -38,9 +41,9 @@ const Header = ({currentUser,hidden}) => (
     </div>
 );
 
-const mapStateToProps=({user: {currentUser},cart:{hidden}})=>({
-    currentUser,
-    hidden
+const mapStateToProps=createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden:selectCartHidden
 });
 
 export default connect(mapStateToProps)(Header);
